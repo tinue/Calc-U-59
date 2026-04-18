1cf2283eb31d630998b7792f75c1ee457620237f
Martin Erzberger <martin@erzberger.ch>
2026-04-18 20:59:04 +0200

    Revert "Fix: prevent calculator stretching horizontally in portrait mode"
    This reverts commit da41e36d85175d1905d9d180cc59706484f85e4b.

--- App/Views/CalculatorView.swift
+++ App/Views/CalculatorView.swift
@@ -117,7 +117,6 @@ struct CalculatorView: View {
                             }
                     } else {
                         calculatorBody(showLabels: showLabels)
-                            .fixedSize(horizontal: true, vertical: false)
                             .overlay(alignment: .topTrailing) {
                                 pageArrow(systemImage: "chevron.right") {
                                     showingPrinter = true
